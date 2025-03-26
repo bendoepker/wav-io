546234da33cd6782b23cf491df2db8adb1de52e9
Ben Doepker <bendoepk@gmail.com>
2025-03-26 08:33:41 -0400

    chagned variable and functions name to standard styles, finished wav medata reading

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+//NOTE:
+//      1:  Some wav files have a pattern on the end that begins with "acid"
+//          this data is big endian encoded and is proprietary to Magix Acid,
+//          however it contains potentially useful data i.e. tempo, meter (4/4),
+//          looping, stretch, disk/ram based, etc.. It may be useful for use in
+//          ByteWave in the future
+//      2:  Zig
+//          Pros
+//              Error Handling
+//              Readability
+//              Better Testing (zig test ...)
+//              Cross platform
+//          Cons
+//              Unstable
+//              Misleading Syntax (Error unions and slices mainly)
+//              Must be coerced to the C ABI (export c...)
+//      3:  C
+//          Pros
+//              Stable
+//              Easily integratable within ByteWave
+//          Cons
+//              Error prone
+//              Needs to be macroed to hell for cross platform ability
+
 static const char* CHUNKID_C = "RIFF";
 static const char* FORMAT_C = "WAVE";
 static const char* SUBCHUNK1ID_C = "fmt ";
@@ -12,28 +36,31 @@ typedef enum error_code {
     E_OK = 0,           //0
     E_FILE_NOT_OPENED,  //1
     E_FILE_READ_FAIL,   //2
-    E_MALFORMED_HEADER, //3
-    E_NON_PCM_DATA,     //4
-    E_FAILED_MALLOC,    //5
+    E_FILE_POS_FAIL,    //3
+    E_MALFORMED_HEADER, //4
+    E_NON_PCM_DATA,     //5
+    E_FAILED_MALLOC,    //6
 } error_code;
 
 typedef struct wav_metadata {
-    int32_t ChunkSize;
-    int32_t SubChunk1Size;
-    int16_t AudioFormat;        //This should be 1 for PCM (Integer) or 3 for IEEE754 (floating point)
-    int16_t NumChannels;
-    int32_t SampleRate;
-    int32_t ByteRate;           //This can be calculated as SampleRate * NumChannels * BitsPerSample/8
-    int16_t BlockAlign;         //This can be calculated as NumChannels * BitsPerSample/8 
+    int32_t chunk_size;         //This is the size of the entire file in bytes -8 bytes (Chunk ID and Chunk Size)
+
+    int16_t audio_format;       //This should be 1 for PCM (Integer) or 3 for IEEE754 (floating point)
+    int16_t num_channels;
+    int32_t sample_rate;
+    int32_t byte_rate;          //This can be calculated as sample_rate * num_channels * bits_per_sample/8
+    int16_t block_align;        //This can be calculated as num_channels * bits_per_sample/8 
                                 //(Number of Bytes for one sample from all channels)
-    int16_t BitsPerSample;
+    int16_t bits_per_sample;
 
-    int16_t ExtraParamSize;     //These ExtraParams are for Non PCM or IEEE754 data (probably will not be used)
-    uint8_t* ExtraParams;       //This needs to be malloced and freed
+    //Not Currently Handling These fields
+    //int16_t extra_param_size; //These ExtraParams are for Non PCM or IEEE754 data (probably will not be used)
+    //uint8_t* extra_params;    //This needs to be malloced and freed
 
-    int32_t SubChunk2Size;      //This is equal to NumSamples * NumChannels * BitsPerSample/8 
                                 //(and should be set to 0 until the file is done being written, then calculated)
-    uint64_t DataPos;           //This is a pointer to the position of the beginning of the data block of the file
+    uint64_t data_pos;          //This is the location (in bytes) of the first sample in the file
+    uint64_t num_samples;       //This is the number of samples in the data block (for stereo audio both
+                                //channels are contained in the same "sample"
 } wav_metadata;
 
 typedef enum tagged_value {
@@ -44,23 +71,14 @@ typedef enum tagged_value {
 typedef struct value_t {
     tagged_value value_type;
     int value_s_len;
-    char* value_s;
+    const char* value_s;
     int32_t value_i;
 } value_t;
 
-error_code read_wav(FILE* input_file, wav_metadata** metadata);
-void formatted_print_header();
-void formatted_print(int64_t offset,
-                     size_t bytes,
-                     char* identifier, int identifier_len,
-                     value_t value);
-
-//NOTE:
-//      1:  Some wav files have a pattern on the end that begins with "acid"
-//          this data is big endian encoded and is proprietary to Magix Acid,
-//          however it contains potentially useful data i.e. tempo, meter (4/4),
-//          looping, stretch, disk/ram based, etc.. It may be useful for use in
-//          ByteWave
+error_code read_wav_metadata(FILE* input_file, wav_metadata** metadata);
+static void formatted_print_header();
+static void formatted_print(int64_t offset, size_t bytes, char* identifier, value_t value);
+void print_metadata(wav_metadata* metadata);
 
 int main() {
     FILE* fp = 0;
@@ -71,150 +89,132 @@ int main() {
     }
 
     wav_metadata* md;
-    error_code res = read_wav(fp, &md);
+    error_code res = read_wav_metadata(fp, &md);
     if(res != E_OK) {
         printf("read_wav failed with error code %d", res);
         return 1;
     }
 
+    print_metadata(md);
+
     free(md);
     fclose(fp);
 }
 
-//TODO: error handling for fgetpos()
-error_code read_wav(FILE* input_file, wav_metadata** metadata) {
+//NOTE: This function uses malloc for @param wav_metadata** but the data needs to be freed elsewhere
+error_code read_wav_metadata(FILE* input_file, wav_metadata** metadata) {
     error_code out = E_OK;
-    *metadata = (wav_metadata*)malloc(sizeof(wav_metadata));
-    int64_t offset = 0;
-    uint64_t bytes = 0;
 
-    //Header ID's
-    char* ChunkID = (char*)malloc(4);
-    char* Format = (char*)malloc(4);
-    char* SubChunk1ID = (char*)malloc(4);
-    char* SubChunk2ID = (char*)malloc(4);
-    if(ChunkID == 0
-        || Format == 0
-        || SubChunk1ID == 0
-        || SubChunk2ID == 0) return E_FAILED_MALLOC;
+    *metadata = (wav_metadata*)malloc(sizeof(wav_metadata));
+    if(*metadata == 0) return E_FAILED_MALLOC;
+
+    //Local Variables
+    int64_t offset = 0;
+    uint64_t items_read = 0;
+
+    char ChunkID[4];
+    char Format[4];
+    char SubChunk1ID[4];
+    char SubChunk2ID[4];
+
+    int32_t sub_chunk_1_size = 0;
+    int32_t sub_chunk_2_size = 0;
 
     if(input_file == 0) return E_FILE_NOT_OPENED;
 
-    //Print the Format Header
-    formatted_print_header();
-
     //Chunk ID
-    fgetpos(input_file, &offset);
-    bytes = fread(ChunkID, sizeof(typeof(*ChunkID)), 4, input_file);
-    if(bytes != sizeof(char) * 4) return E_FILE_READ_FAIL;
+    items_read = fread(ChunkID, sizeof(typeof(*ChunkID)), 4, input_file);
+    if(items_read != sizeof(char) * 4) return E_FILE_READ_FAIL;
     if(strncmp(ChunkID, CHUNKID_C, 4)) return E_MALFORMED_HEADER; //Check that the ID isn't malformed
-    formatted_print(offset, bytes, "Chunk ID", 8, (value_t){string, 4, ChunkID, 0});
 
     //Chunk Size
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->ChunkSize, sizeof(typeof((*metadata)->ChunkSize)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->ChunkSize)); //For formatted print
-    formatted_print(offset, bytes, "Chunk Size (B)", 14, (value_t){integer, 0, 0, (*metadata)->ChunkSize});
-    formatted_print(INT64_MAX, SIZE_MAX, "File Size (B)", 14, (value_t){integer, 0, 0, ((*metadata)->ChunkSize + 8)});
+    items_read = fread(&(*metadata)->chunk_size, sizeof(typeof((*metadata)->chunk_size)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Format
-    fgetpos(input_file, &offset);
-    bytes = fread(Format, sizeof(typeof(*Format)), 4, input_file);
-    if(bytes != (sizeof(char) * 4)) return E_FILE_READ_FAIL;
+    items_read = fread(Format, sizeof(typeof(*Format)), 4, input_file);
+    if(items_read != (sizeof(char) * 4)) return E_FILE_READ_FAIL;
     if(strncmp(Format, FORMAT_C, 4)) return E_MALFORMED_HEADER; //Check that the ID isn't malformed
-    formatted_print(offset, bytes, "Format", 6, (value_t){string, 4, Format, 0});
 
     //Sub Chunk 1 ID
-    fgetpos(input_file, &offset);
-    bytes = fread(SubChunk1ID, sizeof(typeof(*SubChunk1ID)), 4, input_file);
-    if(bytes != 4) return E_FILE_READ_FAIL;
+    items_read = fread(SubChunk1ID, sizeof(typeof(*SubChunk1ID)), 4, input_file);
+    if(items_read != 4) return E_FILE_READ_FAIL;
     if(strncmp(SubChunk1ID, SUBCHUNK1ID_C, 4)) return E_MALFORMED_HEADER; //Check that the ID isn't malformed
-    formatted_print(offset, bytes, "Sub Chunk 1 ID", 14, (value_t){string, 4, SubChunk1ID, 0});
 
     //Sub Chunk 1 Size
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->SubChunk1Size, sizeof(typeof((*metadata)->SubChunk1Size)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->SubChunk1Size)); //For formatted print
-    formatted_print(offset, bytes, "Sub Chunk 1 Size (B)", 20, (value_t){integer, 0, 0, (*metadata)->SubChunk1Size});
+    items_read = fread(&sub_chunk_1_size, sizeof(typeof(sub_chunk_1_size)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Audio Format
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->AudioFormat, sizeof(typeof((*metadata)->AudioFormat)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->AudioFormat)); //For formatted print
-    formatted_print(offset, bytes, "Audio Format (1 = Int, 3 = F32)", 31, (value_t){integer, 0, 0, (*metadata)->AudioFormat});
+    items_read = fread(&(*metadata)->audio_format, sizeof(typeof((*metadata)->audio_format)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Num Channels
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->NumChannels, sizeof(typeof((*metadata)->NumChannels)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->NumChannels)); //For formatted print
-    formatted_print(offset, bytes, "Number of Channels", 18, (value_t){integer, 0, 0, (*metadata)->NumChannels});
+    items_read = fread(&(*metadata)->num_channels, sizeof(typeof((*metadata)->num_channels)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Sample Rate
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->SampleRate, sizeof(typeof((*metadata)->SampleRate)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->SampleRate)); //For formatted print
-    formatted_print(offset, bytes, "Sample Rate (Hz)", 16, (value_t){integer, 0, 0, (*metadata)->SampleRate});
+    items_read = fread(&(*metadata)->sample_rate, sizeof(typeof((*metadata)->sample_rate)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
-    //ByteRate
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->ByteRate, sizeof(typeof((*metadata)->ByteRate)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->ByteRate)); //For formatted print
-    formatted_print(offset, bytes, "Byte Rate (Hz)", 14, (value_t){integer, 0, 0, (*metadata)->ByteRate});
+    //byte_rate
+    items_read = fread(&(*metadata)->byte_rate, sizeof(typeof((*metadata)->byte_rate)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Block Align
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->BlockAlign, sizeof(typeof((*metadata)->BlockAlign)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->BlockAlign)); //For formatted print
-    formatted_print(offset, bytes, "Block Align (B)", 15, (value_t){integer, 0, 0, (*metadata)->BlockAlign});
+    items_read = fread(&(*metadata)->block_align, sizeof(typeof((*metadata)->block_align)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //Bits Per Sample
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->BitsPerSample, sizeof(typeof((*metadata)->BitsPerSample)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->BitsPerSample)); //For formatted print
-    formatted_print(offset, bytes, "Bits Per Sample (b)", 19, (value_t){integer, 0, 0, (*metadata)->BitsPerSample});
+    items_read = fread(&(*metadata)->bits_per_sample, sizeof(typeof((*metadata)->bits_per_sample)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
     //NOTE: This could be a topic for further research...
-    if((*metadata)->SubChunk1Size != 16)
+    //      Not today though
+    if(sub_chunk_1_size != 16)
         return E_NON_PCM_DATA;
 
     //Sub Chunk 2 ID
-    fgetpos(input_file, &offset);
-    bytes = fread(SubChunk2ID, sizeof(typeof(*SubChunk2ID)), 4, input_file);
-    if(bytes != (sizeof(char) * 4)) return E_FILE_READ_FAIL;
+    items_read = fread(SubChunk2ID, sizeof(typeof(*SubChunk2ID)), 4, input_file);
+    if(items_read != (sizeof(char) * 4)) return E_FILE_READ_FAIL;
     if(strncmp(SubChunk2ID, SUBCHUNK2ID_C, 4)) return E_MALFORMED_HEADER; //Check that the ID isn't malformed
-    formatted_print(offset, bytes, "Sub Chunk 2 ID", 14, (value_t){string, 4, SubChunk2ID, 0});
 
     //Sub Chunk 2 Size
-    fgetpos(input_file, &offset);
-    bytes = fread(&(*metadata)->SubChunk2Size, sizeof(typeof((*metadata)->SubChunk2Size)), 1, input_file);
-    if(bytes != 1) return E_FILE_READ_FAIL;
-    bytes *= sizeof(typeof((*metadata)->SubChunk2Size)); //For formatted print
-    formatted_print(offset, bytes, "Sub Chunk 2 Size (B)", 20, (value_t){integer, 0, 0, (*metadata)->SubChunk2Size});
+    items_read = fread(&sub_chunk_2_size, sizeof(typeof(sub_chunk_2_size)), 1, input_file);
+    if(items_read != 1) return E_FILE_READ_FAIL;
 
-    //Free temp data
-    free(ChunkID);
-    free(Format);
-    free(SubChunk1ID);
-    free(SubChunk2ID);
+    //Data Position (using items_read for error handling)
+    items_read = fgetpos(input_file, &offset);
+    if(items_read != 0) return E_FILE_POS_FAIL;
+    (*metadata)->data_pos = offset;
+
+    uint16_t bytes_per_sample = (*metadata)->bits_per_sample / 8;
+
+    //Check Byte Rate Accuracy
+    if((*metadata)->byte_rate !=
+        ((*metadata)->sample_rate * (*metadata)->num_channels * bytes_per_sample))
+        return E_MALFORMED_HEADER;
+
+    //Check Block Align
+    if((*metadata)->block_align !=
+        (*metadata)->num_channels * bytes_per_sample)
+        return E_MALFORMED_HEADER;
+
+    //Calculate Num Samples
+    (*metadata)->num_samples = sub_chunk_2_size /
+        ((*metadata)->num_channels * bytes_per_sample);
+
     return out;
 }
 
-void formatted_print_header() {
+static void formatted_print_header() {
     printf("%-8s | %-8s | %-32s | %-8s\n", "Offset", "Bytes", "Identifier", "Value");
     printf("-------- | -------- | -------------------------------- | -------- \n");
 }
 
 //@param offset: if it is equal to INT64_MAX it will print a blank space
 //@param bytes: if it is equal to SIZE_MAX it will print a blank space
-void formatted_print(int64_t offset, size_t bytes, char* identifier, int identifier_len, value_t value){
+static void formatted_print(int64_t offset, size_t bytes, char* identifier, value_t value){
     //Print the offset (if present)
     if(offset != INT64_MAX)
         printf("%-8lld | ", offset);
@@ -228,7 +228,7 @@ void formatted_print(int64_t offset, size_t bytes, char* identifier, int identif
         printf("         | ");
 
     //Print the Identifier
-    printf("%-32.*s | ", identifier_len, identifier);
+    printf("%-32s | ", identifier);
 
     //Print the Value
     switch(value.value_type) {
@@ -239,4 +239,22 @@ void formatted_print(int64_t offset, size_t bytes, char* identifier, int identif
             printf("%-8d\n", value.value_i);
             break;
     }
+}
+
+void print_metadata(wav_metadata* metadata){
+    formatted_print_header();
+    formatted_print(0, 4, "Chunk ID", (value_t){string, 4, CHUNKID_C, 0});
+    formatted_print(4, 4, "Chunk Size (B)", (value_t){integer, 0, 0, metadata->chunk_size});
+    formatted_print(INT64_MAX, SIZE_MAX, "File Size (B)", (value_t){integer, 0, 0, (metadata->chunk_size + 8)});
+    formatted_print(8, 4, "Format", (value_t){string, 4, FORMAT_C, 0});
+    formatted_print(12, 4, "Sub Chunk 1 ID", (value_t){string, 4, SUBCHUNK1ID_C, 0});
+    formatted_print(20, 2, "Audio Format (1 = Int, 3 = F32)", (value_t){integer, 0, 0, metadata->audio_format});
+    formatted_print(22, 2, "Number of Channels", (value_t){integer, 0, 0, metadata->num_channels});
+    formatted_print(24, 4, "Sample Rate (Hz)", (value_t){integer, 0, 0, metadata->sample_rate});
+    formatted_print(28, 4, "Byte Rate (Hz)", (value_t){integer, 0, 0, metadata->byte_rate});
+    formatted_print(32, 2, "Block Align (B)", (value_t){integer, 0, 0, metadata->block_align});
+    formatted_print(34, 2, "Bits Per Sample (b)", (value_t){integer, 0, 0, metadata->bits_per_sample});
+    formatted_print(36, 4, "Sub Chunk 2 ID", (value_t){string, 4, SUBCHUNK2ID_C, 0});
+    formatted_print(INT64_MAX, SIZE_MAX, "Number of Samples", (value_t){integer, 0, 0, metadata->num_samples});
+    formatted_print(INT64_MAX, SIZE_MAX, "Data Chunk Position", (value_t){integer, 0, 0, metadata->data_pos});
 }
